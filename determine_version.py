5e6737a07e1985a85e6172a04fbf19c4f853d339
aemelius <simoneperfect@gmail.com>
2016-07-29 19:52:26 +0100

    removed unnecessary argument

--- determine_version.py
+++ determine_version.py
@@ -2,13 +2,13 @@ from subprocess import Popen, PIPE
 import re
 
 
-def split(tag, prefix):
-    regex=re.compile("(%s)(\d+)"% prefix)
-    assert regex.match(tag)
-    return regex.match(tag).group(1), int(regex.match(tag).group(2))
+def split(label):
+    regex=re.compile("(.*)(\d+)")
+    assert regex.match(label)
+    return regex.match(label).group(1), int(regex.match(label).group(2))
 
-def next(tag, prefix):
-    prefix, number=split(tag, prefix)
+def next(tag):
+    prefix, number=split(tag)
     return prefix+str(number+1)
 
 # the prefix includes the separator
@@ -21,7 +21,7 @@ def get_last_tag(prefix):
         print line
         if p.match(line):
             tag=p.match(line).group(1)
-            if split(tag, prefix)[1]>split(last, prefix)[1]:
+            if split(tag)[1]>split(last)[1]:
                 last=tag
     return last
 
@@ -33,15 +33,15 @@ def get_next_version(branch):
     result=":-)"
     if "develop"==branch:
         prefix="0.0."
-        return next(get_last_tag(prefix), prefix)
+        return next(get_last_tag(prefix))
     elif release_branch_regex.match(branch):
         first_part=release_branch_regex.match(branch).group(1)
         prefix=first_part+"."
-        return next(get_last_tag(prefix), prefix)
+        return next(get_last_tag(prefix))
     elif feature_branch_regex.match(branch):
         feature_number=feature_branch_regex.match(branch).group(1)
         prefix =  "0.%s."%feature_number
-        return next(get_last_tag(prefix), prefix)
+        return next(get_last_tag(prefix))
     else:
         return None
 
